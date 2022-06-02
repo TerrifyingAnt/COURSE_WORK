db5445eafa9daa2dc558fa2d0754834c01e4e643
Ant <80172256+TerrifyingAnt@users.noreply.github.com>
2022-06-02 16:33:16 +0300

    Добавлена запись книг в хранилище
    Добавлена запись книг в хранилище со страницы автора. Удален добавленный ранее класс NewBook, так как оказался MUDA MUDA MUDA. Книги пока добавляются в формате pdf, хотелось бы либо txt, либо epub.

--- Course_try_1/app/src/main/java/it/mirea/myapplication/AddBookActivity.java
+++ Course_try_1/app/src/main/java/it/mirea/myapplication/AddBookActivity.java
@@ -6,16 +6,13 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,42 +20,49 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import org.w3c.dom.Text;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
-import java.util.Date;
-
-import it.mirea.myapplication.model.Book;
-import it.mirea.myapplication.user.NewBook;
 
 public class AddBookActivity extends Activity {
 
     private static final int SELECT_DOC = 1;
+    private static final int SELECT_IMAGE = 2;
+    private static final int SELECT_METAINF = 3;
     private String selectedDocPath;
-    private TextView bookName;
+    private TextView bookName, choosePhoto, chooseMetaInf;
     private Button chooserButton;
+    private Uri selectedDocUri, selectedImgUri, selectedTxtUri;
     private File file;
     Spinner spinner;
     FirebaseDatabase db;
     DatabaseReference books, users;
+    FirebaseStorage storage;
+    StorageReference storageRef;
+    private String docUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addbook_page);
-        chooserButton = ((Button) findViewById(R.id.chooserButton1));
+        chooserButton = ((Button) findViewById(R.id.chooserButton));
         bookName = findViewById(R.id.bookName);
+        choosePhoto = findViewById(R.id.choosePhoto);
+        chooseMetaInf = findViewById(R.id.chooseMetaInf);
         spinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.category_list, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReferenceFromUrl("gs://library-805f6.appspot.com/");
     }
 
 
@@ -76,22 +80,44 @@ public class AddBookActivity extends Activity {
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent,
                 "Выберите файл"), SELECT_DOC);
+    }
+
+    public void clickAddPhoto(View view) {
+        // in onCreate or any event where your want the user to
+        // select a file
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,
+                "Выберите мем"), SELECT_IMAGE);
+
+    }
+
+    //
+    public void clickAddMetaInf(View view) {
+        // in onCreate or any event where your want the user to
+        // select a file
+        Intent intent = new Intent();
+        intent.setType("text/plain");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,
+                "Выберите файл"), SELECT_METAINF);
 
     }
 
     public void ClickProfile(View view) {
         Intent intent;
-        intent=new Intent(AddBookActivity.this,ProfileActivity.class);
-        switch(new Single().getInstance().type){
+        intent = new Intent(AddBookActivity.this, ProfileActivity.class);
+        switch (new Single().getInstance().type) {
 
             case 0:
-                intent=new Intent(AddBookActivity.this,ProfileActivity.class);
+                intent = new Intent(AddBookActivity.this, ProfileActivity.class);
                 break;
             case 1:
-                intent=new Intent(AddBookActivity.this,ProfileAuthorActivity.class);
+                intent = new Intent(AddBookActivity.this, ProfileAuthorActivity.class);
                 break;
             case 2:
-                intent=new Intent(AddBookActivity.this,ProfileSupervisorActivity.class);
+                intent = new Intent(AddBookActivity.this, ProfileSupervisorActivity.class);
                 break;
         }
         startActivity(intent);
@@ -108,39 +134,52 @@ public class AddBookActivity extends Activity {
         isStoragePermissionGranted();
         if (resultCode == RESULT_OK) {
             if (requestCode == SELECT_DOC) {
-                Uri selectedDocUri = data.getData();
+                selectedDocUri = data.getData();
                 // = getPath(selectedDocUri);
                 file = new File(selectedDocUri.getPath());//create path from uri
                 String path = file.getPath();
                 //String filepath = split[0];//assign it to a string(your choice).
                 bookName.setText(path);
             }
+            if (requestCode == SELECT_IMAGE) {
+                selectedImgUri = data.getData();
+                // = getPath(selectedDocUri);
+                file = new File(selectedImgUri.getPath());//create path from uri
+                String path = file.getPath();
+                //String filepath = split[0];//assign it to a string(your choice).
+                choosePhoto.setText(path);
+
+            }
+            if (requestCode == SELECT_METAINF) {
+                selectedTxtUri = data.getData();
+                file = new File(selectedTxtUri.getPath());
+                String path = file.getPath();
+                chooseMetaInf.setText(path);
+            }
         }
-        //bookName.setText(selectedDocPath);
     }
 
-    public  boolean isStoragePermissionGranted() {
+    public boolean isStoragePermissionGranted() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     == PackageManager.PERMISSION_GRANTED) {
-                Log.v(TAG,"Permission is granted");
+                Log.v(TAG, "Permission is granted");
                 return true;
             } else {
 
-                Log.v(TAG,"Permission is revoked");
+                Log.v(TAG, "Permission is revoked");
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
                 return false;
             }
-        }
-        else { //permission is automatically granted on sdk<23 upon installation
-            Log.v(TAG,"Permission is granted");
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG, "Permission is granted");
             return true;
         }
     }
 
     public void sentToDB(View view) {
-        NewBook book = new NewBook(file, file.getName(), new Single().getInstance().email, spinner.getSelectedItem().toString());
-        db = new Single().getInstance().db;
+
+/*        db = new Single().getInstance().db;
         books = db.getReference("/books");
         users = db.getReference("/");
         books.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
@@ -159,6 +198,36 @@ public class AddBookActivity extends Activity {
             public void onFailure(@NonNull Exception e) {
                 Toast.makeText(AddBookActivity.this, "Хуй!", Toast.LENGTH_SHORT).show();
             }
+        });*/
+        StorageReference pdfRef = storageRef.child("For Checking/" + Uri.fromFile(file).getLastPathSegment());
+        pdfRef.child("logo").putFile(selectedImgUri);
+        pdfRef.child("metainf").putFile(selectedTxtUri);
+        UploadTask uploadTask = pdfRef.child(file.getName()).putFile(selectedDocUri);
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+
+                // Continue with the task to get the download URL
+                return pdfRef.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        Log.e("Doc URL", downloadUri.toString());
+                        docUrl = downloadUri.toString();
+                        Toast.makeText(AddBookActivity.this, "Книга проходит модерацию!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
         });
+        Intent intent = new Intent(AddBookActivity.this, ProfileAuthorActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
